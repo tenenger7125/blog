bd1f0ce5c58551afda81e3b3999ec6321111b69c
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-11 09:16:39 +0900

    [blog@config]: configure SVG files to be used as components

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: config => {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'));
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
