792dfca9f7c10799aa455c7ce12abd279b7bb284
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-12 23:33:53 +0900

    [blog@style]: extend theme(width, maxWidth)

--- src/styles/tailwindcss/extend.ts
+++ src/styles/tailwindcss/extend.ts
@@ -1,3 +1,11 @@
 import { Config } from 'tailwindcss/types/config';
 
-export const extend: Config['theme'] = {};
+export const extend: Config['theme'] = {
+  width: {
+    112: '28rem',
+  },
+  maxWidth: {
+    112: '28rem',
+    '1/2': '50%',
+  },
+};
