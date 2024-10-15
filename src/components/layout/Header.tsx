4ea340ad2e4f317927c1138916d0412530802a3c
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-15 17:12:21 +0900

    [blog@style]: add header title text style

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import { PATH } from '@/constants';
 const Header = () => (
   <header className="sticky top-0 z-[3] mb-3 w-full border-b border-gray-100 bg-white px-3 py-4">
     <div className="m-auto max-w-5xl">
-      <h2>
+      <h2 className="text-2xl font-bold">
         <Link href={PATH.HOME}>Home</Link>
       </h2>
     </div>
