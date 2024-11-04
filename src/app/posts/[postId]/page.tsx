c6c5a22148c0c1447b663f5f4c565e937007cf73
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-04 14:37:35 +0900

    [blog@feat]: add heading text link functionality to sidebar

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -3,9 +3,24 @@ import { markdown } from '@/utils/node/files';
 import 'prismjs/themes/prism-tomorrow.css';
 
 const Post = async ({ params: { postId } }: { params: { postId: string } }) => {
-  const { component } = await markdown.readFile({ id: postId });
+  const { component, headings } = await markdown.readFile({ id: postId });
 
-  return <div className="post prism prose max-w-full">{component}</div>;
+  return (
+    <div className="flex gap-5">
+      <div className="post prism prose max-w-full">{component}</div>
+      <div className="relative">
+        <div className="sticky right-0 top-4 min-w-40">
+          <div className="flex flex-col gap-2">
+            {headings.map(({ title, link }) => (
+              <a key={title} className="block cursor-pointer py-1 hover:text-blue-300" href={link}>
+                {title}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export async function generateStaticParams() {
