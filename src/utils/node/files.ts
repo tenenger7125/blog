ab296b1a7e486135263bdd6b3b9b9a2352770a78
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-04 14:34:50 +0900

    [blog@feat]: add headings info of markdown in files utils

--- src/utils/node/files.ts
+++ src/utils/node/files.ts
@@ -1,15 +1,36 @@
 import fs from 'fs/promises';
 
 import { globby, Options } from 'globby';
+import { Root, Text } from 'mdast';
 import { compileMDX } from 'next-mdx-remote/rsc';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypePrism from 'rehype-prism-plus';
 import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
+import { visit } from 'unist-util-visit';
 
 import { FOLDER_PATH } from '@/constants/node';
 
 import type { Post } from '@/types/post';
+
+const getHeadingsWithHash = () => {
+  const headings: { title: string; link: string }[] = [];
+
+  const extractHeadings = () => (tree: Root) => {
+    visit(tree, 'heading', node => {
+      const text = node.children
+        .filter((child): child is Text => child.type === 'text')
+        .map(child => child.value)
+        .join('');
+      const link = `#${text.split(' ').join('-').toLowerCase()}`;
+
+      headings.push({ title: text, link });
+    });
+  };
+
+  return { extractHeadings, headings };
+};
 
 export const markdown = {
   async getFileNames(options?: Options) {
@@ -21,12 +42,14 @@ export const markdown = {
   async readFile({ cwd, id }: { cwd?: string; id: string }) {
     const fileName = `${id}.md`;
     const source = await fs.readFile(`${cwd ?? FOLDER_PATH.POSTS_ROOT}/${fileName}`, 'utf-8');
+    const { extractHeadings, headings } = getHeadingsWithHash();
+
     const { content, frontmatter } = await compileMDX<Post['metaData']>({
       source,
       options: {
         parseFrontmatter: true,
         mdxOptions: {
-          remarkPlugins: [remarkGfm],
+          remarkPlugins: [remarkParse, extractHeadings, remarkGfm],
           rehypePlugins: [
             rehypeSlug,
             [rehypeAutolinkHeadings, { behavior: 'prepend' }],
@@ -39,6 +62,7 @@ export const markdown = {
     return {
       id,
       source,
+      headings,
       component: content,
       metaData: frontmatter,
     };
