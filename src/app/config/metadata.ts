f62a8661665bd6052609d1bbb29cad68cdfb2e65
LEE DONG GYU <tenenger7125@gmail.com>
2024-11-05 09:58:58 +0900

    [blog@feat]: convert to korean of metadata

--- src/app/config/metadata.ts
+++ src/app/config/metadata.ts
@@ -1,7 +1,7 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'blog',
+  title: '동그라미 블로그',
   description:
-    'This is a blog based on Markdown files, where I organize and share content related to development, such as learning resources and project introductions.',
+    '이 블로그는 마크다운 파일을 기반으로 학습 리소스 및 프로젝트 소개 등 개발과 관련된 콘텐츠를 정리하고 공유하는 블로그입니다.',
 };
