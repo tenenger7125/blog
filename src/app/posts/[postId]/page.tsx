d6a3e8393775096f0efc7f14b18587869e5ff35b
tenenger7125 <tenenger7125@gmail.com>
2025-03-14 15:36:31 +0900

    [blog@style]: shrink post image height thumbnail in post

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -11,7 +11,7 @@ const Post = async ({ params: { postId } }: { params: { postId: string } }) => {
     <div className="flex gap-5">
       <div className="post prism prose max-w-full flex-1">
         {metaData.thumbnail && (
-          <div className="relative my-4 h-160">
+          <div className="relative my-4 h-96 w-full">
             <Image
               alt="thumbnail"
               className="m-0 object-contain"
