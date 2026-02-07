0de4aa8d59997aac039816561e9844e5c99588c6
LEE DONG GYU <tenenger7125@gmail.com>
2026-02-07 23:45:56 +0900

    [blog@fix]: fix dark-mode cookie initial

--- src/app/_components/provider/ClientProvider.tsx
+++ src/app/_components/provider/ClientProvider.tsx
@@ -1,12 +1,23 @@
 'use client';
 
+import { useEffect } from 'react';
+
 import 'prismjs';
 import 'prismjs/plugins/toolbar/prism-toolbar';
 // eslint-disable-next-line import/order
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+import { darkModeStore } from '@/store/dark-mode-store';
 
+import '@/styles/globals.css';
 import 'prismjs/plugins/toolbar/prism-toolbar.min.css';
+import 'prismjs/themes/prism-tomorrow.css';
 
-const ClientProvider = () => null;
+const ClientProvider = () => {
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', darkModeStore.getState().isDarkMode);
+  }, []);
+
+  return null;
+};
 
 export default ClientProvider;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,17 @@
-import 'prismjs';
-
 import { metadata } from '@/app/config/metadata';
 import { Layout } from '@/components';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { STORAGE_KEYS } from '@/constants';
-import { getCookie } from '@/lib/node/cookie';
 
 import ClientProvider from './_components/provider/ClientProvider';
 
-import '@/styles/globals.css';
-import 'prismjs/themes/prism-tomorrow.css';
-
-const RootLayout = async ({ children }: RootLayoutProps) => {
-  const isDarkMode = await getCookie(STORAGE_KEYS.DOTORI_BLOG_DARK_MODE_STORAGE);
-
-  return (
-    <html className={isDarkMode === 'true' ? 'dark' : ''} lang="ko">
-      <ClientProvider />
-      <TooltipProvider delayDuration={0}>
-        <Layout>{children}</Layout>
-      </TooltipProvider>
-    </html>
-  );
-};
+const RootLayout = ({ children }: RootLayoutProps) => (
+  <html lang="ko">
+    <ClientProvider />
+    <TooltipProvider delayDuration={0}>
+      <Layout>{children}</Layout>
+    </TooltipProvider>
+  </html>
+);
 
 interface RootLayoutProps {
   children: React.ReactNode;
