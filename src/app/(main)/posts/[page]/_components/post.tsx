a07de7340b836575953819fd0f991c406c564acc
LEE DONG GYU <tenenger7125@gmail.com>
2026-04-07 13:35:02 +0900

    [blog@style]: add icon of public, unpublic post

--- src/app/(main)/posts/[page]/_components/post.tsx
+++ src/app/(main)/posts/[page]/_components/post.tsx
@@ -1,6 +1,7 @@
-import { Calendar } from 'lucide-react';
+import { Calendar, Lock, Unlock } from 'lucide-react';
 import Link from 'next/link';
 
+import ActionIconButton from '@/components/shared/action-icon-button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { PATH } from '@/constants';
 import { PostDataResponse } from '@/types/post';
@@ -9,7 +10,12 @@ import { excludeImageTag } from '@/utils/sanitize';
 
 const Post = ({ post }: PostProps) => (
   <Link className="w-full" href={`${PATH.POST}/${post.id}`}>
-    <Card className="h-full bg-white text-gray-800 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-400 dark:hover:bg-black">
+    <Card className="relative h-full bg-white text-gray-800 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-400 dark:hover:bg-black">
+      <div className="absolute right-0 top-0 p-3">
+        <ActionIconButton className="px-4 py-2" label={post.published ? '공개' : '비공개'}>
+          {post.published ? <Unlock className="size-5" /> : <Lock className="size-5" />}
+        </ActionIconButton>
+      </div>
       <CardHeader>
         <CardTitle className="line-clamp-1 text-xl">{post.title}</CardTitle>
         <CardDescription></CardDescription>
