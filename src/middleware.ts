e76a54a413b86a9ac16889b30f8b14c034718fc0
LEE DONG GYU <tenenger7125@gmail.com>
2026-04-09 16:10:18 +0900

    [blog@fix]: fix middleware to check auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { COOKIE_KEYS, COOKIE_OPTIONS } from '@/constants/cookie';
+import { COOKIE_KEYS } from '@/constants/cookie';
 
 import { PATH } from './constants';
 import { AUTH_EXEMPT_PATHS, AUTH_REQUIRED_PATHS, AUTH_REQUIRED_REGEX_PATHS } from './constants/auth-path';
@@ -20,23 +20,20 @@ export const middleware = async (request: NextRequest) => {
 
   const isAuthRequired = AUTH_REQUIRED_PATHS.some(path => request.nextUrl.pathname.startsWith(path));
   const isAuthRequiredRegex = AUTH_REQUIRED_REGEX_PATHS.some(regex => regex.test(request.nextUrl.pathname));
-  const isAuthExcluded = AUTH_EXEMPT_PATHS.some(path => request.nextUrl.pathname.startsWith(path));
+  if (isAuthRequired || isAuthRequiredRegex) {
+    const accessToken = request.cookies.get('accessToken')?.value;
+    const validate = await fetchServer(EXTERNAL_URL_IN_NODE.VALIDATE, {
+      method: 'GET',
+      headers: {
+        ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
+      },
+    });
 
-  if (!isAuthRequired && !isAuthRequiredRegex && !isAuthExcluded) {
-    return response; // 인증이 필요한 경로가 아니면 바로 다음으로
-  }
+    if (validate.ok) {
+      return response;
+    }
 
-  let valid = true;
-  const accessToken = request.cookies.get(COOKIE_KEYS.ACCESS_TOKEN)?.value;
-  const validate = await fetchServer(EXTERNAL_URL_IN_NODE.VALIDATE, {
-    method: 'GET',
-    headers: {
-      ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
-    },
-  });
-  if (!validate.ok) {
     const refreshToken = request.cookies.get(COOKIE_KEYS.REFRESH_TOKEN)?.value;
-
     const refreshResult = await fetchServer<ReIssueTokenResponseData>(EXTERNAL_URL_IN_NODE.REFRESH, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -44,28 +41,37 @@ export const middleware = async (request: NextRequest) => {
     });
 
     if (!refreshResult.ok || !refreshResult.data?.accessToken) {
-      response.cookies.delete(COOKIE_KEYS.ACCESS_TOKEN);
-      response.cookies.delete(COOKIE_KEYS.REFRESH_TOKEN);
-      valid = false;
-    } else {
-      response.cookies.set(COOKIE_KEYS.ACCESS_TOKEN, refreshResult.data.accessToken, COOKIE_OPTIONS);
-      response.cookies.set(COOKIE_KEYS.REFRESH_TOKEN, refreshResult.data.refreshToken, COOKIE_OPTIONS);
+      const res = NextResponse.redirect(new URL(PATH.LOGIN, request.url));
+      res.cookies.delete(COOKIE_KEYS.ACCESS_TOKEN);
+      res.cookies.delete(COOKIE_KEYS.REFRESH_TOKEN);
+      return res;
     }
   }
 
-  if ((isAuthRequired || isAuthRequiredRegex) && !valid) {
-    const res = NextResponse.redirect(new URL(PATH.LOGIN, request.url));
+  const isAuthExcluded = AUTH_EXEMPT_PATHS.some(path => request.nextUrl.pathname.startsWith(path));
+  if (isAuthExcluded) {
+    const accessToken = request.cookies.get('accessToken')?.value;
+    const validate = await fetchServer(EXTERNAL_URL_IN_NODE.VALIDATE, {
+      method: 'GET',
+      headers: {
+        ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
+      },
+    });
 
-    res.cookies.delete(COOKIE_KEYS.ACCESS_TOKEN);
-    res.cookies.delete(COOKIE_KEYS.REFRESH_TOKEN);
+    if (validate.ok) {
+      return NextResponse.redirect(new URL(PATH.HOME, request.url));
+    }
 
-    res.headers.set('Cache-Control', 'no-store, max-age=0, must-revalidate');
+    const refreshToken = request.cookies.get(COOKIE_KEYS.REFRESH_TOKEN)?.value;
+    const refreshResult = await fetchServer<ReIssueTokenResponseData>(EXTERNAL_URL_IN_NODE.REFRESH, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ refreshToken }),
+    });
 
-    return res;
-  }
-
-  if (isAuthExcluded && valid) {
-    return NextResponse.redirect(new URL(PATH.HOME, request.url));
+    if (refreshResult.ok) {
+      return NextResponse.redirect(new URL(PATH.HOME, request.url));
+    }
   }
 
   return response;
