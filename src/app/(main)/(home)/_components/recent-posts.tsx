a822116854368255a4a0c072ca625aa8da3f1694
LEE DONG GYU <tenenger7125@gmail.com>
2026-04-18 12:20:36 +0900

    [blog@refactor]: integrate prefetch link to posts page

--- src/app/(main)/(home)/_components/recent-posts.tsx
+++ src/app/(main)/(home)/_components/recent-posts.tsx
@@ -43,8 +43,7 @@ const RecentPosts = async () => {
         <h3 className="text-lg font-bold dark:text-gray-0">최근 게시글</h3>
         <Link
           className="text-sm text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-100"
-          href={PATH.POSTS}
-          prefetch>
+          href={PATH.POSTS}>
           더보기 →
         </Link>
       </div>
