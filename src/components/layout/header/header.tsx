7995ff7623c97c7c0476d4e7a4bff9e359268bd7
LEE DONG GYU <tenenger7125@gmail.com>
2026-04-18 23:22:48 +0900

    [blog@style]: update header hidden if scroll-down

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,39 +1,41 @@
-// 'use client';
+'use client';
 
 import { cn } from 'dotori-utils';
 import Link from 'next/link';
 
 import DarkModeButton from '@/components/shared/dark-mode-button';
 import { PATH } from '@/constants';
+import useScroll from '@/hooks/use-scroll';
 
-const Header = ({ slot }: { slot?: React.ReactNode }) => (
-  // const scroll = useScroll();
+const Header = ({ slot }: { slot?: React.ReactNode }) => {
+  const scroll = useScroll();
 
-  <header className={headerStyle({ hidden: false })}>
-    {/* <header className={headerStyle({ hidden: scroll.isScrollDown })}> */}
-    <div className="max-w-8xl m-auto flex justify-between">
-      <div className="flex items-center gap-6">
-        <h2 className="inline-block font-yOnepickBold text-2xl">
-          <Link className="rounded-lg px-3 hover:bg-gray-300 dark:hover:bg-black" href={PATH.HOME}>
-            동그라미
-          </Link>
-        </h2>
-        <ol className="flex items-center gap-5 font-yOnepickBold text-lg">
-          <li className="h-full">
-            <Link className="h-full rounded-lg px-3 hover:bg-gray-300 dark:hover:bg-black" href={PATH.POSTS}>
-              게시글
+  return (
+    <header className={headerStyle({ hidden: scroll.isScrollDown })}>
+      <div className="max-w-8xl m-auto flex justify-between">
+        <div className="flex items-center gap-6">
+          <h2 className="inline-block font-yOnepickBold text-2xl">
+            <Link className="rounded-lg px-3 hover:bg-gray-300 dark:hover:bg-black" href={PATH.HOME}>
+              동그라미
             </Link>
-          </li>
-        </ol>
-      </div>
+          </h2>
+          <ol className="flex items-center gap-5 font-yOnepickBold text-lg">
+            <li className="h-full">
+              <Link className="h-full rounded-lg px-3 hover:bg-gray-300 dark:hover:bg-black" href={PATH.POSTS}>
+                게시글
+              </Link>
+            </li>
+          </ol>
+        </div>
 
-      <div className="flex items-center gap-4">
-        {slot}
-        <DarkModeButton />
+        <div className="flex items-center gap-4">
+          {slot}
+          <DarkModeButton />
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 const headerStyle = cn(
   'sticky z-[3] w-full border-b dark:border-gray-900 border-gray-100 bg-gray-0 transition-all px-3 py-4 dark:bg-gray-900 dark:text-gray-100',
   {
