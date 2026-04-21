ebdd3f8d74261947235029673f4e87118c545c97
LEE DONG GYU <tenenger7125@gmail.com>
2026-04-22 01:41:13 +0900

    [blog@delete]: delete console code

--- src/app/api/auth/validate/route.ts
+++ src/app/api/auth/validate/route.ts
@@ -1,15 +1,8 @@
-import { NextRequest } from 'next/server';
-
 import { EXTERNAL_URL_IN_NODE } from '@/constants/node/url';
 import { fetchServerWithAuth } from '@/lib/node/fetch-server';
 import { ValidateResponseData } from '@/types/auth';
 
-export async function GET(request: NextRequest) {
-  console.log('validate route invoked', {
-    referer: request.headers.get('referer'),
-    ua: request.headers.get('user-agent'),
-    host: request.headers.get('host'),
-  });
+export async function GET() {
   const result = await fetchServerWithAuth<ValidateResponseData>(EXTERNAL_URL_IN_NODE.VALIDATE, {
     method: 'GET',
   });
