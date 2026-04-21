bdf6ad6ec2e251d8a1b78c82d6bc47d7df0d2a1e
LEE DONG GYU <tenenger7125@gmail.com>
2026-04-22 02:47:52 +0900

    [blog@perf]: improve LCP

--- src/app/config/font.ts
+++ src/app/config/font.ts
@@ -3,9 +3,11 @@ import localFont from 'next/font/local';
 export const pretendardFont = localFont({
   src: '../../assets/fonts/PretendardVariable.ttf',
   variable: '--font-pretendard',
+  display: 'swap',
 });
 
 export const yOnepickBoldFont = localFont({
   src: '../../assets/fonts/YOnepick-Bold.ttf',
   variable: '--font-YOnepick-Bold',
+  display: 'swap',
 });
