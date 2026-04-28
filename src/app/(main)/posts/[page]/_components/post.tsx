20159144c5e66480c8f134f86ee0589258e8bddc
LEE DONG GYU <tenenger7125@gmail.com>
2026-04-28 09:54:53 +0900

    [blog@style]: update post card responsive UI if 640px under

--- src/app/(main)/posts/[page]/_components/post.tsx
+++ src/app/(main)/posts/[page]/_components/post.tsx
@@ -6,30 +6,30 @@ import RelativeDate from '@/components/shared/relative-date';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { PATH } from '@/constants';
 import { PostDataResponse } from '@/types/post';
-import { excludeImageTag } from '@/utils/sanitize';
+import { exclideTags } from '@/utils/sanitize';
 
 const Post = ({ post }: PostProps) => (
   <Link className="w-full" href={`${PATH.POST}/${post.id}`}>
     <Card className="relative h-full bg-white text-gray-800 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-400 dark:hover:bg-black">
       <div className="absolute right-0 top-0 p-3">
-        <ActionIconButton className="px-4 py-2" label={post.published ? '공개' : '비공개'}>
+        <ActionIconButton className="px-4 py-2 text-base" label={post.published ? '공개' : '비공개'}>
           {post.published ? <Unlock className="size-5" /> : <Lock className="size-5" />}
         </ActionIconButton>
       </div>
       <CardHeader>
-        <CardTitle className="line-clamp-1 text-xl">
+        <CardTitle className="line-clamp-1 text-base sm:text-xl">
           {post.category?.name && `[${post.category.name}] `}
           {post.title}
         </CardTitle>
         <CardDescription></CardDescription>
       </CardHeader>
-      <CardContent>
-        <div className="line-clamp-2 h-12" dangerouslySetInnerHTML={{ __html: excludeImageTag(post.content) }} />
+      <CardContent className="h-12">
+        <div className="line-clamp-2 text-sm sm:text-base">{exclideTags(post.content.slice(0, 300))}</div>
       </CardContent>
       <CardFooter>
         <span className="flex items-center gap-1">
-          <Calendar className="size-5" />
-          <span>
+          <Calendar className="size-4 sm:size-5" />
+          <span className="text-sm sm:text-base">
             <RelativeDate date={post.createdAt} />
           </span>
         </span>
--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -4,3 +4,8 @@ export const excludeImageTag = (content: string) =>
   sanitizeHtml(content, {
     allowedTags: sanitizeHtml.defaults.allowedTags.filter(tag => tag !== 'img'),
   });
+
+export const exclideTags = (content: string) =>
+  sanitizeHtml(content, {
+    allowedTags: sanitizeHtml.defaults.allowedTags.filter(tag => !tag),
+  });
